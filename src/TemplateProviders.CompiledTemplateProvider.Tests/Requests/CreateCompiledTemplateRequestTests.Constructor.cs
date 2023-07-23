3f8e9000be392bc4f3b0d0792b036a3da6f3a14e
Paul Deen <paul.deen@dutchworkz.nl>
2023-07-23 16:39:36 +0200

    Add some missing tests

---
+++ src/TemplateProviders.CompiledTemplateProvider.Tests/Requests/CreateCompiledTemplateRequestTests.Constructor.cs
@@ -0,0 +1,39 @@
+﻿namespace TemplateFramework.TemplateProviders.CompiledTemplateProvider.Tests.Requests;
+
+public class CreateCompiledTemplateRequestTests
+{
+    public class Constructor
+    {
+        [Fact]
+        public void Throws_On_Null_AssemblyName()
+        {
+            // Act & Assert
+            this.Invoking(_ => new CreateCompiledTemplateRequest(assemblyName: null!, nameof(Constructor)))
+                .Should().Throw<ArgumentNullException>().WithParameterName("assemblyName");
+        }
+
+        [Fact]
+        public void Throws_On_Empty_AssemblyName()
+        {
+            // Act & Assert
+            this.Invoking(_ => new CreateCompiledTemplateRequest(assemblyName: string.Empty, nameof(Constructor)))
+                .Should().Throw<ArgumentException>().WithParameterName("assemblyName");
+        }
+
+        [Fact]
+        public void Throws_On_Null_ClassName()
+        {
+            // Act & Assert
+            this.Invoking(_ => new CreateCompiledTemplateRequest(GetType().Assembly.FullName!, className: null!))
+                .Should().Throw<ArgumentNullException>().WithParameterName("className");
+        }
+
+        [Fact]
+        public void Throws_On_Empty_ClassName()
+        {
+            // Act & Assert
+            this.Invoking(_ => new CreateCompiledTemplateRequest(GetType().Assembly.FullName!, className: string.Empty))
+                .Should().Throw<ArgumentException>().WithParameterName("className");
+        }
+    }
+}
