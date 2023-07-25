03b409a3236f7df77d27a8c2cc84070de6298ea5
Paul Deen <paul.deen@dutchworkz.nl>
2023-07-25 15:44:15 +0200

    Removed properties from CodeGEnerationSettings after refactoring the interface. (why didn't I get warnings for that?)

--- src/Core.CodeGeneration/CodeGenerationSettings.cs
+++ src/Core.CodeGeneration/CodeGenerationSettings.cs
@@ -2,33 +2,22 @@
 
 public class CodeGenerationSettings : ICodeGenerationSettings
 {
-    public CodeGenerationSettings(string basePath)
-        : this(basePath, false, false, false)
+    public CodeGenerationSettings()
+        : this(false, false)
     {
     }
 
-    public CodeGenerationSettings(string basePath, bool dryRun)
-        : this(basePath, false, false, dryRun)
+    public CodeGenerationSettings(bool dryRun)
+        : this(false, dryRun)
     {
     }
 
-    public CodeGenerationSettings(string basePath, bool generateMultipleFiles, bool dryRun)
-        : this(basePath, generateMultipleFiles, false, dryRun)
+    public CodeGenerationSettings(bool skipWhenFileExists, bool dryRun)
     {
-    }
-
-    public CodeGenerationSettings(string basePath, bool generateMultipleFiles, bool skipWhenFileExists, bool dryRun)
-    {
-        Guard.IsNotNull(basePath);
-
-        BasePath = basePath;
-        GenerateMultipleFiles = generateMultipleFiles;
         SkipWhenFileExists = skipWhenFileExists;
         DryRun = dryRun;
     }
 
-    public string BasePath { get; }
-    public bool GenerateMultipleFiles { get; }
     public bool SkipWhenFileExists { get; }
     public bool DryRun { get; }
 }
