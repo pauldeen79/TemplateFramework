b47d2371c8fe21ac7f48876345fd67178a4c20c3
Paul Deen <paul.deen@dutchworkz.nl>
2023-07-28 11:29:39 +0200

    Fixed build error and integration test

--- src/Core/Extensions/TemplateEngineExtensions.cs
+++ src/Core/Extensions/TemplateEngineExtensions.cs
@@ -2,6 +2,20 @@
 
 public static class TemplateEngineExtensions
 {
-    //public static void Render(this ITemplateEngine instance, IRenderTemplateRequest request)
-    //    => instance.Render(new RenderTemplateRequest<object?>(request));
+    public static void Render(this ITemplateEngine instance, IRenderTemplateRequest request)
+    {
+        Guard.IsNotNull(instance);
+        Guard.IsNotNull(request);
+
+        var type = request.GetType();
+
+        if (type.IsGenericType)
+        {
+            instance.GetType().GetMethod(nameof(ITemplateEngine.Render))!.MakeGenericMethod(type.GetGenericArguments()[0]).Invoke(instance, new[] { request });
+        }
+        else
+        {
+            instance.Render(new RenderTemplateRequest<object?>(request));
+        }
+    }
 }
