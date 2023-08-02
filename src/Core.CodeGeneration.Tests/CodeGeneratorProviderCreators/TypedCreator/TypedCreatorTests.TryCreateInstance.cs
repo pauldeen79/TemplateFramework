38735f8f6cf672a6d61e48d20d14ca2944d441e6
Paul Deen <paul.deen@dutchworkz.nl>
2023-08-02 15:52:38 +0200

    Added tests for new types

---
+++ src/Core.CodeGeneration.Tests/CodeGeneratorProviderCreators/TypedCreator/TypedCreatorTests.TryCreateInstance.cs
@@ -0,0 +1,46 @@
+﻿namespace TemplateFramework.Core.CodeGeneration.Tests.CodeGeneratorProviderCreators;
+
+public partial class TypedCreatorTests
+{
+    public class TryCreateInstance : TypedCreatorTests
+    {
+        [Fact]
+        public void Throws_On_Null_Type()
+        {
+            // Arrange
+            var sut = new TypedCreator();
+
+            // Act
+            sut.Invoking(x => x.TryCreateInstance(type: null!))
+               .Should().Throw<ArgumentNullException>().WithParameterName("type");
+        }
+
+        [Fact]
+        public void Returns_Instance_When_Type_Implements_ICodeGenerationProvider()
+        {
+            // Arrange
+            var sut = new TypedCreator();
+            var type = typeof(MyGeneratorProvider);
+
+            // Act
+            var result = sut.TryCreateInstance(type);
+
+            // Assert
+            result.Should().BeOfType<MyGeneratorProvider>();
+        }
+
+        [Fact]
+        public void Returns_Null_When_Type_Does_Not_Implement_ICodeGenerationProvider()
+        {
+            // Arrange
+            var sut = new TypedCreator();
+            var type = GetType();
+
+            // Act
+            var result = sut.TryCreateInstance(type);
+
+            // Assert
+            result.Should().BeNull();
+        }
+    }
+}
