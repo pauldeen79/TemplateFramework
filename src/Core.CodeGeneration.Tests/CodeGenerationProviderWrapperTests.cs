1b3fba395b4314b5508eb2e54b8e842542d4aae0
Paul Deen <paul.deen@dutchworkz.nl>
2023-08-03 19:14:22 +0200

    Added unit tests for CodeGenerationProviderWrapper, and removed ExcludeFromCodeCoverageAttribute on this class

---
+++ src/Core.CodeGeneration.Tests/CodeGenerationProviderWrapperTests.cs
@@ -0,0 +1,42 @@
+﻿namespace TemplateFramework.Core.CodeGeneration.Tests;
+
+public class CodeGenerationProviderWrapperTests
+{
+    [Fact]
+    public void Throws_On_Null_Instance()
+    {
+        // Act & Assert
+        this.Invoking(_ => new CodeGenerationProviderWrapper(instance: null!))
+            .Should().Throw<ArgumentNullException>().WithParameterName("instance");
+    }
+
+    [Fact]
+    public void Wraps_Instance_Correctly()
+    {
+        // Act
+        var instance = new CodeGenerationProviderWrapper(new MyGeneratorProvider());
+        var compareTo = new MyGeneratorProvider();
+
+        // Assert
+        instance.Path.Should().Be(compareTo.Path);
+        instance.RecurseOnDeleteGeneratedFiles.Should().Be(compareTo.RecurseOnDeleteGeneratedFiles);
+        instance.LastGeneratedFilesFilename.Should().Be(compareTo.LastGeneratedFilesFilename);
+        instance.Encoding.Should().Be(compareTo.Encoding);
+        instance.CreateAdditionalParameters().Should().BeEquivalentTo(compareTo.CreateAdditionalParameters());
+        instance.CreateGenerator().Should().BeOfType(compareTo.CreateGenerator().GetType());
+        instance.CreateModel().Should().BeEquivalentTo(compareTo.CreateModel());
+    }
+
+    [Fact]
+    public void Throws_On_Required_Null_Property()
+    {
+        // Arrange
+        var providerMock = new Mock<ICodeGenerationProvider>();
+        var sut = new CodeGenerationProviderWrapper(providerMock.Object);
+
+        // Act & Assert
+        sut.Invoking(x => _ = x.Path)
+           .Should().Throw<InvalidOperationException>()
+           .WithMessage("Path of template Castle.Proxies.ICodeGenerationProviderProxy was null");
+    }
+}
--- src/Core.CodeGeneration/CodeGenerationProviderWrapper.cs
+++ src/Core.CodeGeneration/CodeGenerationProviderWrapper.cs
@@ -1,47 +1,47 @@
-﻿namespace TemplateFramework.Core.CodeGeneration
+﻿namespace TemplateFramework.Core.CodeGeneration;
+
+internal sealed class CodeGenerationProviderWrapper : ICodeGenerationProvider
 {
-    [ExcludeFromCodeCoverage]
-    internal sealed class CodeGenerationProviderWrapper : ICodeGenerationProvider
+    private readonly object _instance;
+
+    public CodeGenerationProviderWrapper(object instance)
     {
-        private readonly object _instance;
+        Guard.IsNotNull(instance);
 
-        public CodeGenerationProviderWrapper(object instance)
+        _instance = instance;
+    }
+
+    public string Path => NullGuard((string?)_instance.GetType().GetProperty(nameof(Path))?.GetValue(_instance), nameof(Path));
+
+    public bool RecurseOnDeleteGeneratedFiles => NullGuard((bool?)_instance.GetType().GetProperty(nameof(RecurseOnDeleteGeneratedFiles))?.GetValue(_instance), nameof(RecurseOnDeleteGeneratedFiles));
+
+    public string LastGeneratedFilesFilename => NullGuard((string?)_instance.GetType().GetProperty(nameof(LastGeneratedFilesFilename))?.GetValue(_instance), nameof(LastGeneratedFilesFilename));
+
+    public Encoding Encoding => NullGuard((Encoding?)_instance.GetType().GetProperty(nameof(Encoding))?.GetValue(_instance), nameof(Encoding));
+
+    public object? CreateAdditionalParameters() => _instance.GetType().GetMethod(nameof(CreateAdditionalParameters))?.Invoke(_instance, Array.Empty<object>());
+
+    public object CreateGenerator() => NullGuard(_instance.GetType().GetMethod(nameof(CreateGenerator))?.Invoke(_instance, Array.Empty<object>()), nameof(CreateGenerator));
+
+    public object? CreateModel() => _instance.GetType().GetMethod(nameof(CreateModel))?.Invoke(_instance, Array.Empty<object>());
+
+    private T NullGuard<T>(T? value, string name)
+    {
+        if (value is null)
         {
-            _instance = instance;
+            throw new InvalidOperationException($"{name} of template {_instance.GetType().FullName} was null");
         }
 
-        public string Path => NullGuard((string?)_instance.GetType().GetProperty(nameof(Path))?.GetValue(_instance), nameof(Path));
+        return value;
+    }
 
-        public bool RecurseOnDeleteGeneratedFiles => NullGuard((bool?)_instance.GetType().GetProperty(nameof(RecurseOnDeleteGeneratedFiles))?.GetValue(_instance), nameof(RecurseOnDeleteGeneratedFiles));
-
-        public string LastGeneratedFilesFilename => NullGuard((string?)_instance.GetType().GetProperty(nameof(LastGeneratedFilesFilename))?.GetValue(_instance), nameof(LastGeneratedFilesFilename));
-
-        public Encoding Encoding => NullGuard((Encoding?)_instance.GetType().GetProperty(nameof(Encoding))?.GetValue(_instance), nameof(Encoding));
-
-        public object? CreateAdditionalParameters() => _instance.GetType().GetMethod(nameof(CreateAdditionalParameters))?.Invoke(_instance, Array.Empty<object>());
-
-        public object CreateGenerator() => NullGuard(_instance.GetType().GetMethod(nameof(CreateGenerator))?.Invoke(_instance, Array.Empty<object>()), nameof(CreateGenerator));
-
-        public object? CreateModel() => _instance.GetType().GetMethod(nameof(CreateModel))?.Invoke(_instance, Array.Empty<object>());
-
-        private T NullGuard<T>(T? value, string name)
+    private T NullGuard<T>(T? value, string name) where T : struct
+    {
+        if (value is null)
         {
-            if (value is null)
-            {
-                throw new InvalidOperationException($"{name} of template {_instance.GetType().FullName} was null");
-            }
-
-            return value;
+            throw new InvalidOperationException($"{name} of template {_instance.GetType().FullName} was null");
         }
 
-        private T NullGuard<T>(T? value, string name) where T : struct
-        {
-            if (value is null)
-            {
-                throw new InvalidOperationException($"{name} of template {_instance.GetType().FullName} was null");
-            }
-
-            return value.Value;
-        }
+        return value.Value;
     }
 }
