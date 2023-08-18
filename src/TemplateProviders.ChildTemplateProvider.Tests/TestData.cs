d1394c55f66496270777b8f0dc82c036f194f1fe
Paul Deen <paul.deen@dutchworkz.nl>
2023-08-18 13:33:05 +0200

    Fix build

--- src/TemplateProviders.ChildTemplateProvider.Tests/TestData.cs
+++ src/TemplateProviders.ChildTemplateProvider.Tests/TestData.cs
@@ -1,4 +1,7 @@
-﻿namespace TemplateFramework.TemplateProviders.ChildTemplateProvider.Tests;
+﻿using CommunityToolkit.Diagnostics;
+using System.Globalization;
+
+namespace TemplateFramework.TemplateProviders.ChildTemplateProvider.Tests;
 
 internal static class TestData
 {
@@ -23,21 +26,25 @@ internal static class TestData
         public override string ToString() => _delegate(Context);
     }
 
-    internal sealed class MultipleContentBuilderTemplateWithTemplateContextAndTemplateEngine : IMultipleContentBuilderTemplate, ITemplateContextContainer, ITemplateEngineContainer
+    internal sealed class MultipleContentBuilderTemplateWithTemplateContextAndTemplateEngine : IMultipleContentBuilderTemplate, ITemplateContextContainer, ITemplateEngineContainer, ITemplateProviderContainer
     {
         private readonly Action<IMultipleContentBuilder, ITemplateContext, ITemplateEngine, ITemplateProvider> _delegate;
 
-        public MultipleContentBuilderTemplateWithTemplateContextAndTemplateEngine(ITemplateProvider templateProvider,
-                                                                                  Action<IMultipleContentBuilder, ITemplateContext, ITemplateEngine, ITemplateProvider> @delegate)
+        public MultipleContentBuilderTemplateWithTemplateContextAndTemplateEngine(Action<IMultipleContentBuilder, ITemplateContext, ITemplateEngine, ITemplateProvider> @delegate)
         {
-            TemplateProvider = templateProvider;
             _delegate = @delegate;
         }
 
         public ITemplateContext Context { get; set; } = default!;
         public ITemplateEngine Engine { get; set; } = default!;
-        public ITemplateProvider TemplateProvider { get; }
+        public ITemplateProvider Provider { get; set; } = default!;
 
-        public void Render(IMultipleContentBuilder builder) => _delegate(builder, Context, Engine, TemplateProvider);
+        public void Render(IMultipleContentBuilder builder) => _delegate(builder, Context, Engine, Provider);
+    }
+
+    internal class TypeBase
+    {
+        public string Namespace { get; set; } = "";
+        public string Name { get; set; } = "";
     }
 }
