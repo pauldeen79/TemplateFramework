613c91da2bbb1b6ad1b0adc1987c9a1977718945
Paul Deen <paul.deen@dutchworkz.nl>
2023-08-21 16:06:42 +0200

    Added unit tests for TemplateParameterWrapper class

--- src/TemplateProviders.CompiledTemplateProvider/TemplateParameterWrapper.cs
+++ src/TemplateProviders.CompiledTemplateProvider/TemplateParameterWrapper.cs
@@ -18,7 +18,7 @@ public class TemplateParameterWrapper : ITemplateParameter
             var prop = _instance.GetType().GetProperty(nameof(Name));
             if (prop is null)
             {
-                throw new InvalidOperationException("TemplateParameter does not have a property called Name");
+                throw new NotSupportedException("TemplateParameter does not have a property called Name");
             }
             
             return prop.GetValue(_instance)?.ToString() ?? string.Empty;
@@ -32,13 +32,13 @@ public class TemplateParameterWrapper : ITemplateParameter
             var prop = _instance.GetType().GetProperty(nameof(Type));
             if (prop is null)
             {
-                throw new InvalidOperationException("TemplateParameter does not have a property called Type");
+                throw new NotSupportedException("TemplateParameter does not have a property called Type");
             }
 
             var result = prop.GetValue(_instance) as Type;
             if (result is null)
             {
-                throw new InvalidOperationException("Type property did not return a Type instance");
+                throw new NotSupportedException("Type property did not return a Type instance");
             }
 
             return result;
