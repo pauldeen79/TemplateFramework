87c7340b03f5676d8ec4648f84331518f41afb76
Paul Deen <paul.deen@dutchworkz.nl>
2023-08-28 11:17:02 +0200

    Improved unit test for TemplateWrapper after previous added new feature

--- src/TemplateProviders.CompiledTemplateProvider.Tests/TemplateWrapperTests.cs
+++ src/TemplateProviders.CompiledTemplateProvider.Tests/TemplateWrapperTests.cs
@@ -87,6 +87,14 @@ public class TemplateWrapperTests
         {
             public string? Prop1 { get; set; }
             public string? Prop2 { get; set; }
+            public string? Skip1 { get; }
+#pragma warning disable S2376 // Write-only properties should not be used
+#pragma warning disable S3237 // "value" contextual keyword should be used
+#pragma warning disable S108 // Nested blocks of code should not be left empty
+            public string? Skip2 { set { } }
+#pragma warning restore S108 // Nested blocks of code should not be left empty
+#pragma warning restore S3237 // "value" contextual keyword should be used
+#pragma warning restore S2376 // Write-only properties should not be used
         }
     }
 
