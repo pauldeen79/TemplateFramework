303a8ec2654590e81b936e79e72f30cd6a3abc01
Paul Deen <paul.deen@dutchworkz.nl>
2023-09-11 08:50:18 +0200

    Moved some suppressions to unit tests only

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -38,26 +38,4 @@ public static class Program
 
         return app.Execute(args);
     }
-
-    public sealed class MyAssemblyInfoContextService : IAssemblyInfoContextService
-    {
-        public string[] GetExcludedAssemblies() => new[]
-        {
-            "System.Runtime",
-            "System.Collections",
-            "System.ComponentModel",
-            "TemplateFramework.Abstractions",
-            "TemplateFramework.Console",
-            "TemplateFramework.Core",
-            "TemplateFramework.Core.CodeGeneration",
-            "TemplateFramework.Runtime",
-            "TemplateFramework.TemplateProviders.ChildTemplateProvider",
-            "TemplateFramework.TemplateProviders.CompiledTemplateProvider",
-            "TemplateFramework.TemplateProviders.StringTemplateProvider",
-            "CrossCutting.Common",
-            "CrossCutting.Utilities.Parsers",
-            "Microsoft.Extensions.DependencyInjection",
-            "Microsoft.Extensions.DependencyInjection.Abstractions"
-        };
-    }
 }
