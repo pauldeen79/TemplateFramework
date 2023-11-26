c2db61887069884af7809c5ee4ae07c5c7deece0
Paul Deen <paul.deen@dutchworkz.nl>
2023-11-26 16:47:07 +0100

    Added unit test to demonstrate how IndentedStringBuilder works

---
+++ src/Core.Tests/IndentedStringBuilderTests.cs
@@ -0,0 +1,25 @@
+﻿namespace TemplateFramework.Core.Tests;
+
+public class IndentedStringBuilderTests
+{
+    [Fact]
+    public void Just_Works()
+    {
+        // Arrange
+        var sut = new IndentedStringBuilder();
+
+        // Act
+        sut.AppendLine("Hello world");
+        sut.IncrementIndent();
+        sut.AppendLine("Two");
+        sut.DecrementIndent();
+        sut.AppendLine("Back to normal");
+        var actual = sut.ToString();
+
+        // Assert
+        actual.Should().Be(@"Hello world
+    Two
+Back to normal
+");
+    }
+}
