129efc43b2fba6cb68e5ba354b307f926942523d
Paul Deen <paul.deen@dutchworkz.nl>
2024-05-17 14:42:05 +0200

    Minor: Use coalesce expression

--- src/TemplateProviders.CompiledTemplateProvider/ProviderComponent.cs
+++ src/TemplateProviders.CompiledTemplateProvider/ProviderComponent.cs
@@ -23,18 +23,12 @@ public sealed class ProviderComponent : ITemplateProviderComponent
 
         var createCompiledTemplateRequest = (CompiledTemplateIdentifier)identifier;
         var assembly = _assemblyService.GetAssembly(createCompiledTemplateRequest.AssemblyName, createCompiledTemplateRequest.CurrentDirectory);
-        var type = assembly.GetType(createCompiledTemplateRequest.ClassName);
-        if (type is null)
-        {
-            throw new InvalidOperationException($"Could not get type for class {createCompiledTemplateRequest.ClassName}");
-        }
+        var type = assembly.GetType(createCompiledTemplateRequest.ClassName)
+            ?? throw new InvalidOperationException($"Could not get type for class {createCompiledTemplateRequest.ClassName}");
 
-        var template = _templateFactory.Create(type);
-        if (template is null)
-        {
-            throw new InvalidOperationException($"Could not create instance of type {createCompiledTemplateRequest.ClassName}");
-        }
-
+        var template = _templateFactory.Create(type)
+            ?? throw new InvalidOperationException($"Could not create instance of type {createCompiledTemplateRequest.ClassName}");
+        
         return template;
     }
 }
