550712f914b739188e00ec32876af7d32e5abba1
Paul Deen <paul.deen@dutchworkz.nl>
2024-09-02 12:48:18 +0200

    Added missing unit tests

---
+++ src/TemplateProviders.ChildTemplateProvider.Tests/Extensions/TemplateEngineExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿namespace TemplateFramework.TemplateProviders.ChildTemplateProvider.Tests.Extensions;
+
+public class TemplateEngineExtensionsTests
+{
+    public class RenderChildTemplates
+    {
+        [Fact]
+        public async Task Without_Context_Renders_ChildTemplates_Correctly()
+        {
+            // Arrange
+            var templateEngine = Substitute.For<ITemplateEngine>();
+            var generationEnvironment = Substitute.For<IGenerationEnvironment>();
+            IEnumerable childModels = new object[] { new object(), new object(), new object() };
+
+            // Act
+            await templateEngine.RenderChildTemplates(childModels, generationEnvironment, CancellationToken.None);
+
+            // Assert
+            await templateEngine.Received(3).Render(Arg.Any<IRenderTemplateRequest>(), CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task With_Context_Renders_ChildTemplates_Correctly()
+        {
+            // Arrange
+            var templateEngine = Substitute.For<ITemplateEngine>();
+            var generationEnvironment = Substitute.For<IGenerationEnvironment>();
+            var templateContext = Substitute.For<ITemplateContext>();
+            IEnumerable childModels = new object[] { new object(), new object(), new object() };
+
+            // Act
+            await templateEngine.RenderChildTemplates(childModels, generationEnvironment, templateContext, CancellationToken.None);
+
+            // Assert
+            await templateEngine.Received(3).Render(Arg.Any<IRenderTemplateRequest>(), CancellationToken.None);
+        }
+    }
+
+    public class RenderChildTemplate
+    {
+        [Fact]
+        public async Task Without_Context_Renders_ChildTemplates_Correctly()
+        {
+            // Arrange
+            var templateEngine = Substitute.For<ITemplateEngine>();
+            var generationEnvironment = Substitute.For<IGenerationEnvironment>();
+            object childModel = new object();
+
+            // Act
+            await templateEngine.RenderChildTemplate(childModel, generationEnvironment, CancellationToken.None);
+
+            // Assert
+            await templateEngine.Received(1).Render(Arg.Any<IRenderTemplateRequest>(), CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task With_Context_Renders_ChildTemplates_Correctly()
+        {
+            // Arrange
+            var templateEngine = Substitute.For<ITemplateEngine>();
+            var generationEnvironment = Substitute.For<IGenerationEnvironment>();
+            var templateContext = Substitute.For<ITemplateContext>();
+            object childModel = new object();
+
+            // Act
+            await templateEngine.RenderChildTemplate(childModel, generationEnvironment, templateContext, CancellationToken.None);
+
+            // Assert
+            await templateEngine.Received(1).Render(Arg.Any<IRenderTemplateRequest>(), CancellationToken.None);
+        }
+    }
+}
